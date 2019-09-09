ecdcda1062d327bec19a6995381df40c9741cfdd
Your Name <you@example.com>
2019-09-09 13:33:29 +0530

    Database Connection Php File

---
+++ database_connection.php
@@ -0,0 +1,10 @@
+<?php
+//database_connection.php
+//db = adbms
+//username = root
+//password = ''
+
+$connect = new PDO('mysql:host=localhost;dbname=asset_management_db', 'root', '');
+session_start();
+
+?>
