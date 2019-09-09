80c6af2a5fac02cb401f5d924d8adac69d38746f
Your Name <you@example.com>
2019-09-09 13:38:34 +0530

    databse password updated

--- database_connection.php
+++ database_connection.php
@@ -2,7 +2,7 @@
 //database_connection.php
 //db = adbms
 //username = root
-//password = ''
+//password = 'Test'
 
 $connect = new PDO('mysql:host=localhost;dbname=asset_management_db', 'root', '');
 session_start();
