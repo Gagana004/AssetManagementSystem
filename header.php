6a4c8807c92be11edba2b3fb3214268e4e09d785
Gagana Kavindu Gamage <37533941+Gagana004@users.noreply.github.com>
2019-09-17 15:31:25 +0530

    main pages

--- header.php
+++ header.php
@@ -1,10 +1,56 @@
 <?php
-// header.php
- ?>
-
- <!DOCTYPE html>
- <html>
+//header.php
+?>
+<!DOCTYPE html>
+<html>
 	<head>
-		<title></title>
+		<title>Asset Management System</title>
+		
+		<!-- CSS -->
+		<link rel="stylesheet" href="css/bootstrap.min.css" />
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+		<link rel="stylesheet" href="includes/customCSS.css"/>
+		<link rel="stylesheet" href="css/dataTables.bootstrap.min.css" />
+
+		<!-- JS -->
+		<script src="js/jquery.dataTables.min.js"></script>
+		<script src="js/dataTables.bootstrap.min.js"></script>		
+		<script src="js/jquery-1.10.2.min.js"></script>
+		<script src="js/bootstrap.min.js"></script>
+		
 	</head>
 	<body>
+		<br />
+		<div class="container">
+			<h2 align="center">Asset Management System</h2>
+
+			<nav class="navbar navbar-inverse">
+				<div class="container-fluid">
+					<div class="navbar-header">
+						<a href="index.php" class="navbar-brand">Home</a>
+					</div>
+					<ul class="nav navbar-nav">
+						<li><a href="vendor.php">Vendor</a></li>
+						<li><a href="category.php">Category</a></li>
+						<li><a href="item.php">Items </a></li>
+						<li><a href="purchase.php">Purchase</a></li>
+						<li><a href="asset.php">Assets</a></li>
+						<li><a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> Asset Transfer </a>
+       						 <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+					          <a class="dropdown-item" href="transfer.php">Transfer</a>
+					          <a class="dropdown-item" href="repair.php">Repair</a>
+					          <a class="dropdown-item" href="disposal.php">Disposal</a>
+					        </div>
+        				</li>												
+					</ul>
+					<ul class="nav navbar-nav navbar-right">
+						<li class="dropdown">
+							<ul class="dropdown-menu">
+								<li><a href="profile.php">Profile</a></li>
+								<li><a href="logout.php">Logout</a></li>
+							</ul>
+						</li>
+					</ul>
+				</div>
+			</nav>
+			 
