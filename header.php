c5cd511b37ae3f1bfe4bef49bd91df44f6466efb
Manisha Gunaratne <ManishaGun@users.noreply.github.com>
2019-09-18 13:34:23 +0530

    session handling

--- header.php
+++ header.php
@@ -54,6 +54,7 @@
 					</ul>
 					<ul class="nav navbar-nav navbar-right">
 						<li class="dropdown">
+							<a href="#" class="dropdown-toggle" data-toggle="dropdown"><span class="label label-pill label-danger count"></span> <?php echo $_SESSION["fname"]; ?></a>
 							<ul class="dropdown-menu">
 								<li><a href="profile.php">Profile</a></li>
 								<li><a href="logout.php">Logout</a></li>
